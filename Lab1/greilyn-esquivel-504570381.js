c6ba768d1809afa40a2aab5828417d77727ebcbd
GreilynES <greilynesquivel@gmail.com>
2024-03-09 15:17:34 -0600

    lab 1 palidromo

---
+++ Lab1/greilyn-esquivel-504570381.js
@@ -0,0 +1,21 @@
+function esPalindromo (phrase){
+    //convertir a minusculas y remover caracteres no alfabeticos incluyendo 
+    phrase = phrase.toLowerCase().replace(/\s /g, '');
+
+    //Revierte la cadena
+    let splittedPhrase = phrase. split('');
+    let reversePhrase = ' ';
+
+    //Recorrer el Array 'cadenaEnArray' desde el ultimo al primer elemento 
+    //Almacenar el string revertido en la variable  revertedString
+    for(let i=splittedPhrase-1; i>=0; i--){
+
+        reversePhrase += splittedPhrase[i];
+    }
+
+        return splittedPhrase == reversePhrase;
+    
+    
+    }
+    console.log(esPalindromo("Anita lava la tina")); // true
+    console.log(esPalindromo("Hola mundo")); // false
