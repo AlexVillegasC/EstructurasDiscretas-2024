772e9a4239429488b0319e6ee9b3787e1ae25147
Brandon-Nunez-Corrales <brandon16corrales@gmail.com>
2024-03-09 15:29:59 -0600

    feature

--- Lab1/alex-villegas-503990937.js
+++ Lab1/alex-villegas-503990937.js
@@ -147,5 +147,6 @@ function esPalindromo(phrase)
    return phrase === revertedPhrase;
 }
 
-console.log(esPalindromo("Anita lava la tina")); // true.
-console.log(esPalindromo("Hola mundo")); // false
+
+console.log(esPalindromo("Anita lava la tina")); // true
+console.log(esPalindromo("Hola mundo")); // falsee
