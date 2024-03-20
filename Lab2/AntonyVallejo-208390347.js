c5c3cc15e2337fe6a54b4dee49733ef237c6208a
Antonyvallejo <antonyvallejo933@gmail.com>
2024-03-20 14:59:25 -0600

    Laboratorio 2

---
+++ Lab2/AntonyVallejo-208390347.js
@@ -0,0 +1,11 @@
+//Las variables declaradas con var tienen un alcance de función. 
+//Esto significa que están disponibles dentro de la función en la que se declaran, 
+//independientemente del bloque en el que se encuentren.
+
+
+function Ejemplovar() {
+    var number=100;
+    console.log(number);
+}
+
+
