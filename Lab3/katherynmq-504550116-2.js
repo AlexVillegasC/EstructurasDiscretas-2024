b5a0313ead4fe98ad210b3abb757bbcf4fb30645
KaMendez04 <katherynmq@gmail.com>
2024-03-23 14:35:29 -0600

    parte 2

---
+++ Lab3/katherynmq-504550116-2.js
@@ -0,0 +1,34 @@
+const calificaciones = [7, 8, 5, 9, 10, 8, 6, 7, 9];
+sortGrades(calificaciones);
+console.log(calificaciones); // Output: [10, 9, 9, 8, 8, 7, 7, 6, 5]
+
+
+function sortGrades(calificaciones) {
+    let n = calificaciones.length;
+    for (let i = 0; i < n-1; i++) {
+      for (let j = 0; j < n-i-1; j++) {
+        if (calificaciones[j] < calificaciones[j+1]) {
+          let temp = calificaciones[j];
+          calificaciones[j] = calificaciones[j+1];
+          calificaciones[j+1] = temp;
+        }
+      }
+    }
+  }
+
+  function bubbleSort(arr) {
+    const n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (arr[j] < arr[j + 1]) {
+          const temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+  }
+
+
+
+  console.log(`Las cinco calificaciones más altas son: ${calificaciones.slice(0,5)}`);
