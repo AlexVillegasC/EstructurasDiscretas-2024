8a7314aa5b5b1f2def6d32b8a1f0c7ec560098b7
GreilynES <greilynesquivel@gmail.com>
2024-03-23 14:24:36 -0600

    greilynesquivel-parte1

---
+++ Lab3/greilynesquivel-504570381-1.js
@@ -0,0 +1,20 @@
+const libros = [
+    {id: 105, titulo: "El principito"},
+    {id: 210, titulo: "Cien años de soledad"},
+    {id: 304, titulo: "Don Quijote de la Mancha"},
+    {id: 457, titulo: "1984"},
+    {id: 501, titulo: "Un mundo feliz"}
+    // ...
+]
+console.log(searchId(105));
+
+function searchId(searchLibro){
+    for(let i =0; i < libros.length; i++){
+        if(libros[i].id == searchLibro)
+        {
+            return libros[i].titulo;
+        }
+    }
+
+    return "No encuetrado";
+}
