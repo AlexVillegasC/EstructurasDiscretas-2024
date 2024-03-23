aa12465f471669d8c3ce5f80f61c924f7c0363bf
= <alexvillegascarranza@gmail.com>
2024-03-23 14:39:46 -0600

    lab 3 update

--- Lab3/alexvillegas-503990937-1.js
+++ Lab3/alexvillegas-503990937-1.js
@@ -1,1 +1,29 @@
 // Lineal Search
+
+const libros = [
+    {id: 105, titulo: "El principito"},
+    {id: 210, titulo: "Cien años de soledad"},
+    {id: 304, titulo: "Don Quijote de la Mancha"},
+    {id: 457, titulo: "1984"},
+    {id: 501, titulo: "Un mundo feliz"}
+    // ...
+]
+
+console.log(libros[0].titulo);
+
+var nombres = ["pedro", "juan", "daniel"];
+
+console.log(searchName("daniel"));
+
+function searchName(searchCriteria)
+{
+    for(let i = 0; i  < nombres.length;  i++)
+    {
+        if(nombres[i] == searchCriteria)
+        {
+            return "Si se encuentra";
+        }
+    }
+
+    return "No se encuentra";
+}
