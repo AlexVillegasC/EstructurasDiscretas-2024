1596eff371fc99b5294692a0217a3606f01f64b8
Braythonn <braytonbra@gmail.com>
2024-04-04 23:22:32 -0600

    lab2-fix

---
+++ Lab2/braythonchassagne-119130354.js
@@ -0,0 +1,21 @@
+// ejemplos con let 
+let x = 5;
+console.log(x);
+
+let y = 10; 
+console.log(y);
+
+// ejemplos con const
+const nombre = "Bray";
+console.log(nombre);
+
+const edad = 20;
+console.log(edad);
+
+//ejemplo con var
+var reloj = 0;
+reloj = reloj + 1;
+console.log(reloj);
+
+var notificacion = "hogaa";
+console.log(notificacion);
