28bdc0e1ef58c13e6621f694e956176702ac9ffb
KaMendez04 <katherynmq@gmail.com>
2024-04-13 14:11:45 -0600

    ssd

--- Lab4/katherynmendez-504550116-1.js
+++ Lab4/katherynmendez-504550116-1.js
@@ -5,27 +5,31 @@ class Queue{
 
     EnQueue(element) {
         this.array.push(element);
-        console.log(this.array);
     }
 
     DeQueue(){
         this.array.shift();
-        console.log(this.array);
-      }
+    }
+
+      PrintOut(){
+        console.log(this.array+ "\n");
+    }
 }
 
 const myQueue = new Queue(); //instancia de la clase
 
 //Agregar elementos
-console.log("Agregando elementos....\n");
+console.log("Agregando elementos....");
 myQueue.EnQueue(5);
 myQueue.EnQueue(23);
 myQueue.EnQueue(3);
 myQueue.EnQueue(71);
 myQueue.EnQueue(4);
+myQueue.PrintOut();
 
 console.log("FIFO...")
 myQueue.DeQueue();
+myQueue.PrintOut();
 
 //Este procedimiento se repite, hasta que la cola se acabe. Por demostracion, solo hice el procedimiento una vez. 
 //Tomando en cuenta que aunque este saliendo un elemento, es posible que a su vez, entren mas procedimientos con segundos de diferencia..
