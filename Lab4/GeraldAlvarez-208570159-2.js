fd61d2ed072ffc18e3f0b59c5eb87ce0ebef1a2e
Honguito77 <geraldalvarez972@gmail.com>
2024-04-13 15:03:46 -0600

    lab-4

---
+++ Lab4/GeraldAlvarez-208570159-2.js
@@ -0,0 +1,29 @@
+// Task
+
+class Task{
+    constructor(){
+        this.Array = [];
+    }
+    push(element){
+
+         this.Array.push(element);
+         console.log(this.Array);
+       
+    }
+    pop(){
+        this.Array.pop();
+        console.log(this.Array);
+        
+    }
+    taskeach(){
+        this.Array.forEach(element => {
+            console.log(element);
+        });
+    }
+}
+const task = new Task();
+task.push(1);
+task.push(2);
+task.push(3);
+task.pop();
+task.taskeach();
