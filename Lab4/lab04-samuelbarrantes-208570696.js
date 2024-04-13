51f233c0fb0077bbf1d0d38fc24ac8585908e3b0
saMu <samuelbarrantes1@gmail.com>
2024-04-13 15:05:10 -0600

    lab04-samu

---
+++ Lab4/lab04-samuelbarrantes-208570696.js
@@ -0,0 +1,37 @@
+//Queue
+
+//firts in 
+// firts out
+
+    class Queue{
+        constructor(){
+            this.Array = [];
+        }
+        push(element){
+
+             this.Array.push(element);
+             console.log(this.Array);
+           
+        }
+        shift(){
+            this.Array.shift();
+            console.log(this.Array);
+            
+        }
+        queue(){
+            this.Array.forEach(element => {
+                console.log(element);
+            });
+        }
+    }
+    const newQueue = new Queue();
+    newQueue.push(1);
+    newQueue.push(2);
+    newQueue.push(3);
+    newQueue.shift();
+    newQueue.queue();
+    
+ 
+ 
+   
+
