4ebea1cc097590896fd7c84bf668e4f6da7e27c0
JOSE ROMAN FERNANDEZ <jose.roman.fernandez@est.una.ac.cr>
2024-04-14 21:03:28 -0600

    add-lab04-joseroman-504560520

---
+++ Lab4/joseroman-504560520-1.js
@@ -0,0 +1,33 @@
+//Queue
+
+class Queue{
+    constructor(){
+        this.array = [];
+    }
+
+    EnQueue(_element){
+        this.array.push(_element);
+        //console.log(this.array);/.
+    }
+
+    DeQueue(){
+        let deQueue = this.array.shift();
+        console.log("Se elimino " + deQueue)
+        //console.log(this.array);
+    }
+
+    PrintQueue(){
+       this.array.forEach(element => {
+            console.log(element);
+       });
+        
+    }
+}
+
+const queue = new Queue();
+queue.EnQueue(4);
+queue.EnQueue(3);
+queue.EnQueue(2);
+queue.PrintQueue();
+queue.DeQueue();
+queue.PrintQueue();
