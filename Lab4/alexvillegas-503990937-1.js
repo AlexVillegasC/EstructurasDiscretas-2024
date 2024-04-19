ecb8450037ff36f886d54660fa8d37d16fd9d950
= <alexvillegascarranza@gmail.com>
2024-04-19 00:54:58 -0600

    Merge with Dev + Lab 4 changes

--- Lab4/alexvillegas-503990937-1.js
+++ Lab4/alexvillegas-503990937-1.js
@@ -1,1 +1,38 @@
 // Queue
+
+
+// FIRST IN
+// FIRST OUT
+class Queue{
+
+    constructor()
+    {
+        this.array = [];
+    }
+
+    Enqueue(element)
+    {
+        this.array.push(element);
+    }
+
+   Dequeue()
+   {
+       this.array.shift(); 
+   }
+
+   printQueue()
+   {
+        this.array.forEach(element => {
+            console.log(element);
+        });
+   }
+}
+
+const cola = new Queue();
+cola.Enqueue(1);
+cola.Enqueue(2);
+cola.Enqueue(3);
+
+cola.Dequeue();
+
+cola.printQueue();
