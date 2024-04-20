a41ee6a762061da370cb89375f39d3c63be25e3a
KaMendez04 <katherynmq@gmail.com>
2024-04-20 13:41:39 -0600

    j

--- Lab5/504550116.js
+++ Lab5/504550116.js
@@ -4,13 +4,13 @@
 // 2. **O(Log N) - Complejidad Logarítmica:**
 //    - Implementa un algoritmo que divide repetidamente la cantidad de datos en cada paso.   
 
-// 3. **O(N) - Complejidad Lineal:**
+// 3. **O(N) - Complejidad Lineal:** Gnome sort en el mejor de los casos
 //    - Implementa un algoritmo que recorra cada elemento de la entrada una sola vez.   
 
 // 4. **O(N Log N) - Complejidad Log-Lineal:**
 //    - Implementa un algoritmo que combine un proceso lineal con uno logarítmico.   
 
-// 5. **O(N^2) - Complejidad Cuadrática:**
+// 5. **O(N^2) - Complejidad Cuadrática:** Gnome Sort, en el peor de los casos
 //    - Implementa un algoritmo donde el tiempo de ejecución es proporcional al cuadrado del tamaño de la entrada.
 
 // 6. **O(2^N) - Complejidad Exponencial:**
