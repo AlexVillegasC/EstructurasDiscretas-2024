a18d1670e94c81bfbde5c6376b30dc918676fd2b
DannyOr94 <danny.abarca.alvarado@est.una.ac.cr>
2024-05-03 17:03:15 -0600

    Lab3 listo

---
+++ Lab3/Danny-Abarca-504000385-1.js
@@ -0,0 +1,25 @@
+var id;
+var titulo; 
+
+const libros =
+[
+    {id: 105, titulo: "El principito"},
+    {id: 210, titulo: "Cien años de soledad"},
+    {id: 304, titulo: "Don Quijote de la Mancha"},
+    {id: 457, titulo: "1984"},
+    {id: 501, titulo: "Un mundo feliz"},
+]
+
+console.log(searchId (457));
+
+function searchId(searchID)
+{
+    for (let i = 0; i < libros.length; i++)
+    {
+        if (libros[i].id == searchID)
+        {
+            return libros[i].titulo;
+        }
+    }
+    return "Nose encuentra";
+} 
