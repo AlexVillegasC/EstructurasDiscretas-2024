3e6bdbad681fc1abf995a490b4750f4ce05e04a2
Billy <billyugalde2004@gmail.com>
2024-05-18 14:23:14 -0600

    Lab 7

---
+++ Lab7/Billy-Ugalde-Villagra-504570551.js
@@ -0,0 +1,30 @@
+function romanoAEntero(romano) {
+    const mapaRomano = new Map([
+        ['I', 1],
+        ['V', 5],
+        ['X', 10],
+        ['L', 50],
+        ['C', 100],
+        ['D', 500],
+        ['M', 1000]
+    ]);
+
+    let total = 0;
+    for (let i = 0; i < romano.length; i++) {
+        const valorActual = mapaRomano.get(romano[i]);
+        const valorSiguiente = mapaRomano.get(romano[i + 1]);
+
+        if (valorSiguiente && valorActual < valorSiguiente) {
+            total += valorSiguiente - valorActual;
+            i++;
+        } else {
+            total += valorActual;
+        }
+    }
+    return total;
+}
+
+// Ejemplo de uso
+let numeroRomano = "MCMXCIV";
+let entero = romanoAEntero(numeroRomano);
+console.log(`El valor entero de ${numeroRomano} es ${entero}`);
