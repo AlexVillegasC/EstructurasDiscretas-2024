8527ed6fa2e2297a191f358e6c8c6f7199c69e83
Antonyvallejo <antonyvallejo933@gmail.com>
2024-05-18 13:51:26 -0600

    Lab 6

--- Practica-Examen/practica.js
+++ Practica-Examen/practica.js
@@ -61,11 +61,11 @@
 //     for (let i = 0; i < length; i++) {
 //         for (let j = 0; j < length - i - 1; j++) {
 //             if (array[j] < array[j + 1]) {
-//                 const temp = array[j];
-//                 array[j] = array[j + 1];
-//                 array[j + 1] = temp;
-//             }
-//         }
+//                const temp = array[j];
+//                array[j] = array[j + 1];
+//                array[j + 1] = temp;
+//            }
+//        }
 //     }
-//     return array;
-// }
+//    return array;
+//
