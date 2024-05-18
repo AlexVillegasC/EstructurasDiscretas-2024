a379c4001cb756848b75060fad8d9ba5d4d44b08
Billy <billyugalde2004@gmail.com>
2024-05-18 14:52:11 -0600

    Lab7

--- Lab7/Billy-Ugalde-Villagra-504570551.js
+++ Lab7/Billy-Ugalde-Villagra-504570551.js
@@ -24,7 +24,7 @@ function romanoAEntero(romano) {
     return total;
 }
 
-// Ejemplo de uso
+// Ejemplo 
 let numeroRomano = "MCMXCIV";
 let entero = romanoAEntero(numeroRomano);
 console.log(`El valor entero de ${numeroRomano} es ${entero}`);
