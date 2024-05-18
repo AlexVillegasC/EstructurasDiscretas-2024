a357c1b319191c58c4a9292552ef3387421499fe
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2024-05-18 13:23:25 -0600

    ..

---
+++ Lab5/frander-504440503.js
@@ -0,0 +1,48 @@
+// 1. **O(1) - Complejidad Constante:**
+//    - Implementa un algoritmo que siempre ejecute una cantidad fija de pasos, sin importar el tamaño de la entrada.   
+
+function ComplejidadConstante (numero) 
+{
+    return numero;
+}
+
+
+// 2. **O(Log N) - Complejidad Logarítmica:**
+//    - Implementa un algoritmo que divide repetidamente la cantidad de datos en cada paso.   
+
+
+
+// 3. **O(N) - Complejidad Lineal:**
+//    - Implementa un algoritmo que recorra cada elemento de la entrada una sola vez.   
+
+// 4. **O(N Log N) - Complejidad Log-Lineal:**
+//    - Implementa un algoritmo que combine un proceso lineal con uno logarítmico.   
+
+// 5. **O(N^2) - Complejidad Cuadrática:**
+//    - Implementa un algoritmo donde el tiempo de ejecución es proporcional al cuadrado del tamaño de la entrada.
+
+// 6. **O(2^N) - Complejidad Exponencial:**
+//    - Implementa un algoritmo que crezca exponencialmente con el tamaño de la entrada.   
+
+// 7. **O(N!) - Complejidad Factorial:**
+//    - Implementa un algoritmo cuyo número de pasos crece factorialmente con el tamaño de la entrada.
+
+
+var entrada=1; 
+
+switch(entrada)
+{
+    case 1:{ // caso de complejidad constante 
+        let num = 11; 
+        console.log("Numero: " + ComplejidadConstante(num));
+        //  regresa el mismo dato que le ingresa porque se hace en un solo paso...
+    break;}
+
+    case 2:{ // case de complejidad logaritmica
+
+
+    break;}
+
+
+}
+
--- Practica-Examen/practica.js
+++ Practica-Examen/practica.js
@@ -55,17 +55,21 @@
 
 // nombre();
 
-// function desconocido(array) {
-//     const length = array.length;
+function desconocido(array) {
+    const length = array.length;
 
-//     for (let i = 0; i < length; i++) {
-//         for (let j = 0; j < length - i - 1; j++) {
-//             if (array[j] < array[j + 1]) {
-//                 const temp = array[j];
-//                 array[j] = array[j + 1];
-//                 array[j + 1] = temp;
-//             }
-//         }
-//     }
-//     return array;
-// }
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length - i - 1; j++) {
+            if (array[j] < array[j + 1]) {
+                const temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+let lista = [1,2,3,4,5,6]
+
+desconocido(lista);
