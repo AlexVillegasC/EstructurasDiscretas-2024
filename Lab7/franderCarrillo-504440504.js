cbd19e6f7a7e64482aba52456ad4cf02ed4feba8
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2024-05-18 14:33:48 -0600

    ejemplo lab7

---
+++ Lab7/franderCarrillo-504440504.js
@@ -0,0 +1,27 @@
+function eliminarDuplicados(arr) {
+    let mapa = new Map();
+
+    arr.forEach(elemento => {
+        if (mapa.has(elemento))
+         {
+            mapa.set(elemento, mapa.get(elemento) + 1);
+        } 
+        else 
+        {
+            mapa.set(elemento, 1);
+        }
+    });
+
+// array para los que no se repiten 
+    let unicos = [];
+    mapa.forEach((valor, clave) => {
+        unicos.push(clave);
+    });
+
+    return unicos;
+}
+
+// Ejemplo
+let arr = [1, 2, 3, 1, 2, 3, 4, 5, 6, 4, 5, 6, 7, 8, 9, 7, 7, 8, 9];
+let arrSinIguales = eliminarIguales(arr);
+console.log(arrSinIguales);
