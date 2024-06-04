4a4945e7a9a46eef3ad07a38c0762ea1b034f416
SoyMaloLoSeShh <jose.nunez.villalobos@est.una.cr>
2024-06-04 13:48:49 -0600

    Lab9

---
+++ Lab9/danielnunez-119040930.js
@@ -0,0 +1,48 @@
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        // Construcción hard-codeada del árbol
+        this.root = new TreeNode(5);
+        this.root.left = new TreeNode(3);
+        this.root.right = new TreeNode(6);
+        this.root.left.left = new TreeNode(2);
+        this.root.left.right = new TreeNode(4);
+        this.root.left.left.left = new TreeNode(1);
+    }
+
+    inorderSuccessor(root, n) {
+        let successor = null;
+        let current = root;
+
+        while (current !== null) {
+            if (current.val > n.val) {
+                successor = current;
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return successor;
+    }
+}
+
+// Crear el árbol
+let bst = new BinarySearchTree();
+
+// Nodo para el cual buscamos el sucesor inorden
+let nodeToFindSuccessor = new TreeNode(2);  // Debemos crear un nodo con el valor buscado
+
+// Encontrar y mostrar el sucesor inorden
+let successor = bst.inorderSuccessor(bst.root, nodeToFindSuccessor);
+if (successor !== null) {
+    console.log("El sucesor inorden de " + nodeToFindSuccessor.val + " es: " + successor.val);
+} else {
+    console.log("No hay sucesor inorden para " + nodeToFindSuccessor.val);
+}
